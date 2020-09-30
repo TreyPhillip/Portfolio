687e0d1e9ad74e32bd124f781bfc434d5ca2b9aa
TreyPhillip <treyphillip@live.com>
2020-09-30 16:24:48 -0400

    restructure app to class based

--- src/App.js
+++ src/App.js
@@ -1,26 +1,39 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import './styles/App.scss';
+import Sidebar from './Sidebar';
+import Main from './Main';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+class App extends Component {
+  constructor() {
+      super();
+      this.state = {
+        collapsed: true,
+        toggled: true,
+      };
+    this.handleToggleSidebar = this.handleToggleSidebar.bind(this);
+  }
+
+  handleToggleSidebar = (value) => {
+    this.setState({
+        toggled: !value,
+        collapsed: this.state.toggled
+    });
+  }
+
+  render() {
+    return (
+      <div className={`app ${this.state.toggled ? 'toggled' : ''}`}>
+        <Sidebar 
+          toggled={this.state.toggled}
+          collapsed={this.state.collapsed}
+          handleToggleSidebar={this.handleToggleSidebar}
+        />  
+        <Main 
+          handleToggleSidebar={this.handleToggleSidebar}
+        />
+      </div>       
+    );
+  } this
+};
 
 export default App;
