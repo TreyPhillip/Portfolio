2a2ca671034b9337d1e4a34a276fd710e03e53b3
TreyPhillip <treyphillip@live.com>
2020-10-02 16:27:23 -0400

    create project list page

---
+++ src/components/Projects/Projects.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+
+class Projects extends Component {
+    render() {
+        return (
+            <main>
+                <header>
+                    <h1>My Projects</h1>
+                </header>
+                <div>
+                    <p></p>
+                </div>
+            </main>
+        );
+    };
+}
+
+export default Projects;
