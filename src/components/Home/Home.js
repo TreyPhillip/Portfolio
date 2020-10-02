3b033d34e5033b992691a606fcc6bd70869062da
TreyPhillip <treyphillip@live.com>
2020-10-02 16:27:10 -0400

    create home page

---
+++ src/components/Home/Home.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+class Home extends Component {
+    render() {
+        return (
+            <main>
+                <header className="intro">
+                    <h1>Hi, I'm Trey. A junior developer</h1>
+                </header>
+            </main>
+        );
+    };
+}
+
+export default Home;
