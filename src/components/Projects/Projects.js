e6de77e5b9819c835a6d168f3944b6a29c857c47
TreyPhillip <treyphillip@live.com>
2020-10-06 21:25:11 -0400

    added display of ProjectList

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -1,16 +1,28 @@
 import React, { Component } from 'react';
+import { ProjectList } from './ProjectList/ProjectList';
+import VisualizerImage1 from '../images/VisualizerImage1.jpg'
 
 class Projects extends Component {
+    constructor() {
+        super();
+        this.state = {
+            projects: [
+                {projectid: '1', title: 'pathfindingVisualizer', description: 'pathfinding visualizer' , image: VisualizerImage1, github: 'https://github.com'},
+                {projectid: '2', title: 'pathfindingVisualizer2', description: 'pathfinding visualizer', image: VisualizerImage1, github: 'https://github.com'},
+            ]
+        };
+    }
+    
     render() {
         return (
-            <main>
-                <header>
-                    <h1>My Projects</h1>
-                </header>
-                <div>
-                    <p></p>
-                </div>
-            </main>
+            <div>
+                <main>
+                    <header className="head">
+                        <h1>My Projects</h1>
+                    </header>
+                </main>
+                    <ProjectList project={this.state.projects} />               
+            </div>
         );
     };
 }
