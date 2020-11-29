d5230e8eb26250a777f4a4d2b2c0f0b9e1f432e4
TreyPhillip <treyphillip@live.com>
2020-11-29 16:41:08 -0500

    implementing hooks and functional components

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { Component } from "react";
+import React, { useState } from "react";
 import { Route, HashRouter, Switch } from 'react-router-dom';
 import './styles/App.scss';
 import Sidebar from './components/Sidebar/Sidebar';
@@ -8,56 +8,43 @@ import About from './components/About/About';
 import Contact from './components/Contact/Contact';
 import { FaBars } from 'react-icons/fa';
 
-class App extends Component {
-  constructor() {
-      super();
-      this.state = {
-        collapsed: true,
-        toggled: false,
-      };
-    this.handleToggleSidebar = this.handleToggleSidebar.bind(this);
-    document.title = 'Trey Phillip | Junior Developer';
+// const handleToggleSidebar = (value) => {
+//   setToggled(!toggled);
+//   setCollapsed(!collapsed);
+// }
+
+export default function App() {
+  const [toggled, setToggled] = useState(true);
+  const [collapsed, setCollapsed] = useState(true);
+
+  const handleToggleSidebarMobile = (value) => {
+    setToggled(!value);
+    setCollapsed(toggled);
   }
 
-  handleToggleSidebar = (value) => {
-    this.setState({
-        toggled: !value,
-        collapsed: this.state.toggled
-    });
-  }
+  document.title = 'Trey Phillip | Junior Developer';
 
-  handleToggleSidebarMobile = (value) => {
-    this.setState({
-        toggled: !value,
-        collapsed: true
-    });
-  }
-
-  render() {
-    return (
-      <HashRouter basename={process.env.PUBLIC_URL}>
-        <div className={`app ${this.state.toggled ? 'toggled' : ''}`}>        
-            <Sidebar
-              toggled={this.state.toggled}
-              collapsed={this.state.collapsed}
-              handleToggleSidebar={this.handleToggleSidebar}
-            /> 
-            <main>
-              <div className="btn-toggle" onClick={() => this.handleToggleSidebarMobile(this.state.toggled)}>
-                <FaBars />
-              </div>          
-              <Switch>     
-                  <Route path="/Home" component={Home}/>
-                  <Route exact path="/" component={Home} />
-                  <Route path="/Projects" component={Projects} />
-                  <Route path="/About" component={About} />
-                  <Route path="/Contact" component={Contact} />
-              </Switch>
-            </main>
-        </div>
-      </HashRouter>
-    );
-  } this
-};
-
-export default App;
+  return (
+    <HashRouter basename={process.env.PUBLIC_URL}>
+      <div className={`app ${toggled ? 'toggled' : ''}`}>        
+          <Sidebar
+            toggled={toggled}
+            collapsed={collapsed}
+            handleToggleSidebar={handleToggleSidebarMobile}
+          /> 
+          <main>
+            <div className="btn-toggle" onClick={() => handleToggleSidebarMobile(toggled)}>
+              <FaBars />
+            </div>          
+            <Switch>     
+                <Route path="/Home" component={Home}/>
+                <Route exact path="/" component={Home} />
+                <Route path="/Projects" component={Projects} />
+                <Route path="/About" component={About} />
+                <Route path="/Contact" component={Contact} />
+            </Switch>
+          </main>
+      </div>
+    </HashRouter>
+  );
+}
--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -1,36 +1,31 @@
-import React, { Component } from 'react';
+import React, { useState, useEffect } from 'react';
 import ProjectList from './ProjectList/ProjectList';
 import VisualizerImage1 from '../images/VisualizerImage1.jpg';
 import DiceImage1 from '../images/DiceImage1.jpg';
 import PortfolioImage1 from '../images/PortfolioImage1.jpg';
 
-class Projects extends Component {   
-    constructor() {
-        const DiceDescription = 'Discord bot developed using JavaScript and the Discord.js node module';
-        const PathfindingDescription = 'Pathfinding Visualizer in Python using the A* algorithm';
-        const PortfolioDescription = 'A website that acts as my portfolio. The website you\'re on right now!';
-        super();
-        this.state = {
-            projects: [
-                {projectid: '1', title: 'Path-finding Visualizer', description: PathfindingDescription, image: VisualizerImage1, github: 'https://github.com/TreyPhillip/PathfindingVisualizer'},
-                {projectid: '2', title: 'MrDiceGuy', description: DiceDescription, image: DiceImage1, github: 'https://github.com/TreyPhillip/MrDiceGuy'},
-                {projectid: '3', title: 'My Porfolio', description: PortfolioDescription, image: PortfolioImage1, github: 'https://github.com/TreyPhillip/TreyPhillip.github.io'},
-            ]
-        };
-    }
+export default function Projects() {   
+    const DiceDescription = 'Discord bot developed using JavaScript and the Discord.js node module';
+    const PathfindingDescription = 'Pathfinding Visualizer in Python using the A* algorithm';
+    const PortfolioDescription = 'A website that acts as my portfolio. The website you\'re on right now!';
     
-    render() {
-        return (
-                <main>
-                    <header className="head">
-                        <h1>My Projects</h1>
-                    </header>
-                    <div className="projects">
-                        <ProjectList project={this.state.projects} />
-                    </div>
-                </main>                    
-        );
-    };
-}
+    const [projects, setProjects] = useState([]);
 
-export default Projects;
+    useEffect(() => {
+        setProjects([
+            {projectid: '1', title: 'Path-finding Visualizer', description: PathfindingDescription, image: VisualizerImage1, github: 'https://github.com/TreyPhillip/PathfindingVisualizer'},
+            {projectid: '2', title: 'MrDiceGuy', description: DiceDescription, image: DiceImage1, github: 'https://github.com/TreyPhillip/MrDiceGuy'},
+            {projectid: '3', title: 'My Porfolio', description: PortfolioDescription, image: PortfolioImage1, github: 'https://github.com/TreyPhillip/TreyPhillip.github.io'},
+        ])
+    }, [])
+    return (
+        <main>
+            <header className="head">
+                <h1>My Projects</h1>
+            </header>
+            <div className="projects">
+                <ProjectList project={projects} />
+            </div>
+        </main>                    
+    );
+};
