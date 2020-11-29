0207c41b83c71def341333671343bb668de39cb9
TreyPhillip <treyphillip@live.com>
2020-11-29 16:43:14 -0500

    change to functional component and edit about

--- src/components/Contact/Contact.js
+++ src/components/Contact/Contact.js
@@ -1,33 +1,28 @@
-import React, { Component } from 'react';
+import React from 'react';
 
-class Contact extends Component {
-    render() {
-        const email = <a href={"mailto: treyphillip@live.com"}>treyphillip@live.com</a>;
-        const twitter = <a href={"https://twitter.com/RealTreyPhillip"}>Twitter</a>;
-        const linkedin = <a href={"https://www.linkedin.com/in/treyphillip/"}>LinkedIn</a>;
-        return (
-            <main>
-                <header className="head">
-                    <h1>Contact Me</h1>
-                </header>
-                <p className="block"> 
-                    I am currently looking for long term employment, but I am also 
-                    interested in freelance work, so feel free to send me an email.
-                    If you would like to get in contact with me, im available on the 
-                    platforms below.
-                    <br />
-                    <br />
-                    {email}
-                    <br />
-                    <br />
-                    {twitter}
-                    <br />
-                    <br />
-                    {linkedin}
-                </p>
-            </main>
-        );
-    }
+export default function Contact() {
+    const email = <a href={"mailto: treyphillip@live.com"}>treyphillip@live.com</a>;
+    const twitter = <a href={"https://twitter.com/RealTreyPhillip"}>Twitter</a>;
+    const linkedin = <a href={"https://www.linkedin.com/in/treyphillip/"}>LinkedIn</a>;
+    return (
+        <main>
+            <header className="head">
+                <h1>Contact Me</h1>
+            </header>
+            <p className="block"> 
+                I am currently looking for long term employment, but I am also 
+                interested in freelance work, so feel free to send me a message on
+                one of the platforms below.
+                <br />
+                <br />
+                {email}
+                <br />
+                <br />
+                {twitter}
+                <br />
+                <br />
+                {linkedin}
+            </p>
+        </main>
+    );
 }
-
-export default Contact;
