051ca9506976e389bedde11a8e70d63878e76a97
Trey Phillip <Treyphillip@live.com>
2021-06-09 15:09:59 -0400

    added detail page for portfolio project

--- src/components/Projects/ProjectDetail/Portfolio/Portfolio.js
+++ src/components/Projects/ProjectDetail/Portfolio/Portfolio.js
@@ -4,8 +4,22 @@ export default function PathFinder() {
     return (
         <main>
             <header className="head">
-                <h1>Under Construction</h1>
+                <h1>My Portfolio</h1>
             </header>
+            <p className="block">
+                I built this portfolio as a small demonstration of my knowledge in Web Design, 
+                JavaScript and React.js. Its also used as a platform to display information about 
+                the other projects I have worked on
+            </p>
+            <header className="head">
+                <h1>How was it built?</h1>
+            </header>
+            <p className="block">
+                The app is currently just a React.js app hosted on GitHub Pages, but I have 
+                plans to add some features that will may require a backend (most likely Node.js).
+                Some examples of those featutes include a Blog, comment system and a simple 
+                authentication with google or another third party system to make those comments.
+            </p>
         </main>
         );
     }
